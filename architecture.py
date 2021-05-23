d99efa20b2452dcce4f0c7dd5794c184ea93b779
hackerspacealbania <uljan89@gmail.com>
2021-05-23 19:56:38 +0100

    adding iot architecture model

---
+++ architecture.py
@@ -0,0 +1,39 @@
+"""
+
+Example of cloud-based and edge computing-based IoT architectures
+"""
+
+def cloud():
+    """Encapsulation of all networks and its connected devices"""
+
+    gateways = 'backbone gateways'
+    edge_layers = 'cloudlet'
+    local_gateways = 'local fog gateway'
+
+    print('what does cloud do?')
+
+def gateway():
+    """Gateway layer"""
+
+    backbone = 'what are the backbone gateways?'
+
+    local = 'what do local gateways do?'
+   
+
+def iot_node():
+    """IoT node layer"""
+
+    smart_appliances = 'TV, Home automation, Sensor node, smart agri'
+
+    print('what is the iot node layer role?')
+
+def users():
+    """Remote user interface"""
+
+    web='web based interface'
+    mobile = 'mobile interface'
+    computer = 'computer based interface'
+
+    print('user login, view, control devices')
+
+
