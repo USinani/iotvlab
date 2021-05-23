7ce3fe8ea427d15d5c4a40468f389219b2b5ad3f
hackerspacealbania <uljan89@gmail.com>
2021-05-23 19:34:00 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # iotvlab
 Virtual Lab for Internet of Things
+
+# Installation and setup
+./setup.py
+docker
+
+
+# Tools and Platforms
+Shodan
+
---
+++ iotsec.py
@@ -0,0 +1,18 @@
+# security of IoT devices
+
+def smart_home():
+    camera='stream video'
+    smart_speaker = 'i stream voice'
+    smart_sensors = 'i listen and stream to central device'
+
+def central_sys():
+    """I listen to device communications at home"""
+
+    listen = 'open ports'
+    identify = 'who is how'
+
+    speaker = 'turn on the speaker'
+    devices = 'router, camera, speakers'
+
+    print (' {devices} online communicating at {port}')
+
